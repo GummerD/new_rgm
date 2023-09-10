cf4bbd4d545b25510cf96ea19275096bb829d8ef
Artem <armrt@mail.ru>
2023-09-10 03:10:39 +0300

    Настроена Миграция для таблицы "tasks"

--- database/migrations/2023_09_09_235028_create_tasks_table.php
+++ database/migrations/2023_09_09_235028_create_tasks_table.php
@@ -6,22 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('tasks', function (Blueprint $table) {
+      $table->bigIncrements('id');
+      $table->string('level');
+      $table->string('level_name');
+      $table->integer('num_task')->unsigned();
+      $table->text('task_text');
+      $table->text('text')->nullable();
+      $table->text('correct_answer');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('tasks');
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('tasks');
+  }
 };
