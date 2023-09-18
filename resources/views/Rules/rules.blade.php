19f28584db484f7c6f4f4a276237f12500877c1c
ira2198 <83193516+ira2198@users.noreply.github.com>
2023-09-18 23:15:29 +0300

    верстка rules,profile

--- resources/views/Rules/rules.blade.php
+++ resources/views/Rules/rules.blade.php
@@ -8,12 +8,12 @@
 
 <main class="contain">
 
-  <h3>Правила:</h3>
+  <h3 class='rules_title' style="font-weight:bold">Правила:</h3>
 
   <div class="rules_content">
     <div class="text_rules">
-      <h2>Что такое регулярныне выражения</h2>
-      <p style="font-size: 18px">
+      <h4>Что такое регулярныне выражения</h4>
+      <p style="font-size: 14px">
         Регулярное выражение – способ записи текстовых шаблонов. По
         сути, это обычная текстовая строка написанная на специальном
         языке, но она не содержит
@@ -21,28 +21,28 @@
         такие:
         строка из 3 символов, первый символ a.
       </p>
-      <p style="font-size: 18px">
+      <p style="font-size: 14px">
         Структура регулярного выражения выглядит следующим образом:
         строка которая всегда начинается с символа разделителя,
         за ним следует шаблон регулярного выражения,
         затем еще один символ разделителя и, наконец, необязятельный список модификаторов.
       </p>
-      <p style="font-size: 18px">
+      <p style="font-size: 16px">
         Простой пример визульного представления рег. выражения: /[a-b^w]/
       </p>
     </div>
     <div class="ref-info">
 
-      <h2> Основные правилa языка регулярных выражений:</h2>
-      <table class="table table-bordered">
-        <tr>
-          <th style="font-size: 20px">Выражение</th>
-          <th style="font-size: 20px">Описание</th>
+      <h4> Основные правилa языка регулярных выражений:</h4>
+      <table class="table">
+        <tr class="">
+          <th class="rules_table_th" style="font-size: 18px">Выражение</th>
+          <th style="font-size: 18px">Описание</th>
         </tr>
         @foreach ($rules as $rule)
         <tr>
-          <td style="font-weight:lighter;font-size:22px;color:rgb(68, 43, 24)">{{ $rule->regex }}</td>
-          <td style="font-size: 18px;font-style:italic">{{ $rule->description }}</td>
+          <td class='table_rule_regex rules_table_th'>{{ $rule->regex }}</td>
+          <td class="" style="font-size: 16px;font-style:italic">{{ $rule->description }}</td>
         </tr>
         @endforeach
       </table>
